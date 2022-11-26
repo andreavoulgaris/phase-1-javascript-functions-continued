6186266956fd0fd1d1b0e5656ade5a8eb7c38f3b
Andrea Voulgaris <abv@umich.edu>
2022-11-25 20:43:27 -0500

    commit tree cd94dae0f326d6a82bf4e81d8a8f01ce25116694

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = "roller-skate"
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity) {
+    if (activity === undefined) {
+        activity = "go to the office"
+    }
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair, innerFunction) {
+    if (visualFlair === undefined) {
+        visualFlair = "*"
+    }
+    innerFunction()
+}
+
+const innerFunction = function(special) { 
+    if (special === undefined) {
+        special = "special"
+    }
+    return `${visualFlair} You are ${special} ! ${visualFlair}`
+}
